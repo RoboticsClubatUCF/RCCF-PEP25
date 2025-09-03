a659a41de2bf073b144fbff356cf4e25857fe1c7
mbapple <junkmbfl10000@gmail.com>
2025-09-03 17:24:46 +0900

    Removed extra folder

--- ArduRover Stuff/Lua Scripts/arm_disarm_relay.lua
+++ ArduRover Stuff/Lua Scripts/arm_disarm_relay.lua
@@ -3,11 +3,13 @@
 
 function update_relay_pwm()
     if arming:is_armed() then
-        SRV_Channels:set_output_pwm_chan_timeout(5, 2000, 100) 
-        gcs:send_text(6, "ARMED: Relay ON (2000 PWM on AUX OUT 1)")
+        --SRV_Channels:set_output_pwm_chan_timeout(5, 2000, 100) 
+        gpio:write(105, 1)
+        gcs:send_text(6, "ARMED: Relay ON")
     else
-        SRV_Channels:set_output_pwm_chan_timeout(5, 1000, 100)
-        gcs:send_text(6, "DISARMED: Relay OFF (1000 PWM on AUX OUT 1)")
+        --SRV_Channels:set_output_pwm_chan_timeout(5, 1000, 100)
+        gpio:write(105, 0)
+        gcs:send_text(6, "DISARMED: Relay OFF")
     end
 end
 
